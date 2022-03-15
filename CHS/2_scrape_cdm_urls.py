a195c24f111b78f20063907fe98225bdde02fd67
rl1987 <rl1987@users.noreply.github.com>
2022-03-15 17:58:20 +0200

    Write out overriden URL in 2_scrape_cdm_urls.py

--- CHS/2_scrape_cdm_urls.py
+++ CHS/2_scrape_cdm_urls.py
@@ -63,6 +63,7 @@ def main():
 
             out_row = dict(in_row)
             out_row["cdm_url"] = cdm_url
+            out_row['web_url'] = web_url
 
             pprint(out_row)
             csv_writer.writerow(out_row)
