730fa0f1814e7667a4535d881b238831bb19f71a
rl1987 <rl1987@users.noreply.github.com>
2022-03-21 20:12:16 +0200

    Generate SQL commands to update hospitals table

--- cdmpricing/4_set_ccns.py
+++ cdmpricing/4_set_ccns.py
@@ -4,6 +4,7 @@ import csv
 from glob import glob
 from pprint import pprint
 import sys
+import os
 
 FIELDNAMES = [
     "cms_certification_num",
@@ -32,10 +33,36 @@ def main():
 
     in_f.close()
 
+    cdm_id_to_site = dict()
+
+    in_f = open("moz-inbound-links-for-cdmpricing_com-2022-03-21_09_06_07_330067Z.csv", "r")
+    csv_reader = csv.reader(in_f)
+
+    for row in csv_reader:
+        if len(row) != 17:
+            continue
+
+        cdm_url = row[7]
+        if cdm_url == "Target URL":
+            continue
+
+        print(cdm_url)
+        cdm_id = cdm_url.split('/')[1]
+
+        web_url = row[0]
+        web_url = "https://" + web_url.split("/")[0]
+
+        if cdm_id != "":
+            cdm_id_to_site[cdm_id] = web_url
+
+    in_f.close()
+
     pprint(cdm_id_to_ccn)
 
+    h_f = open("hospitals.sql", "w")
+
     for f in glob("*.csv"):
-        if f.startswith("moz-") or f == "ccns.csv":
+        if f.startswith("moz-") or f == "ccns.csv" or f.startswith("out_"):
             continue
 
         in_f = open(f, "r", encoding="utf-8")
@@ -47,10 +74,19 @@ def main():
         csv_writer = csv.DictWriter(out_f, fieldnames=FIELDNAMES, lineterminator="\n")
         csv_writer.writeheader()
 
+        not_found = False
+
+        ccn = None
+        cdm_id = None
+
         for in_row in csv_reader:
-            ccn = cdm_id_to_ccn.get(in_row.get('cdm_id'))
+            cdm_id = in_row.get("cdm_id")
+            ccn = cdm_id_to_ccn.get(cdm_id)
             if ccn is None:
-                continue
+                out_f.close()
+                os.unlink("out_" + f)
+                not_found = True
+                break
 
             out_row = dict(in_row)
 
@@ -61,7 +97,14 @@ def main():
 
             csv_writer.writerow(out_row)
 
-        out_f.close()
+        if not not_found:
+            out_f.close()
+    
+            if cdm_id is not None and ccn is not None:
+                web_url = cdm_id_to_site.get(cdm_id)
+                h_f.write('UPDATE `hospitals` SET `homepage_url` = "{}", `chargemaster_url` = "https://www.cdmpricing.com/{}/standard-charges", `last_edited_by_username` = "rl1987" WHERE `cms_certification_num` = "{}";\n'.format(web_url, cdm_id, ccn))
+
+    h_f.close()
 
 if __name__ == "__main__":
     main()
