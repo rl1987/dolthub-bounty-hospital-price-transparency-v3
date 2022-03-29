96235f4fc353ecffca747fa25cf38040d063ffe8
rl1987 <rl1987@users.noreply.github.com>
2022-03-29 19:14:26 +0300

    Install DO agent for additional monitoring

--- provision.sh
+++ provision.sh
@@ -6,3 +6,6 @@ curl -L https://github.com/dolthub/dolt/releases/latest/download/install.sh > /t
 dolt config --global --add user.email rimantas@keyspace.lt
 dolt config --global --add user.name "rl1987"
 pip3 install openpyxl requests lxml js2xml doltpy xlrd
+
+curl -sSL https://repos.insights.digitalocean.com/install.sh -o /tmp/install.sh
+bash /tmp/install.sh
